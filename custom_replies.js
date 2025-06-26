951c6e456f60089337bd5ca5042fbe13cbba3990
iskov92 <ap.iskov92@gmail.com>
2025-06-27 02:45:53 +0700

    Исправлено сообщение о времени ожидания в пользовательских шаблонах ответов, убрано избыточное слово "ответа".

--- custom_replies.js
+++ custom_replies.js
@@ -85,7 +85,7 @@
                 <div class="my-6">
                     <p class="replay-text-block cursor-pointer line-height-tight mb-1">Благодарю за оценку и обратную связь по вашему вопросу. Если у вас появятся дополнительные вопросы, пожалуйста, напишите нам. Будем рады помочь.</p>
                     <p class="replay-text-block cursor-pointer line-height-tight mb-1">Благодарю за обратную связь по вашему вопросу. Если у вас появятся дополнительные вопросы, пожалуйста, напишите нам. Будем рады помочь.</p></p>
-                    <p class="replay-text-block cursor-pointer line-height-tight mb-1">Для ответа на ваш вопрос потребуется дополнительное время. Пожалуйста, ожидайте ответа.</p>
+                    <p class="replay-text-block cursor-pointer line-height-tight mb-1">Для ответа на ваш вопрос потребуется дополнительное время. Пожалуйста, ожидайте.</p>
                 </div>
                 <div style="margin-top:auto; display:flex; justify-content:flex-end;">
                     <button type="button" class="v-btn v-btn--flat v-theme--lightTheme v-btn--density-compact v-btn--size-default v-btn--variant-elevated text--secondary mr-2 cancel-btn" style="border:1px solid #9e9e9e; color:#9e9e9e; background:transparent;"><span class="v-btn__content">Отмена</span></button>
